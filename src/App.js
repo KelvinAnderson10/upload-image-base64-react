abe6c84136eb4479865f2c4c0435612d238168ef
Kelvin Anderson <kelvinanders17@gmail.com>
2022-08-26 09:35:58 +0700

    [DONE]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
-function App() {
+const UploadAndDisplayImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState("")
+  let reader = new FileReader();
+  const ref = useRef(null)
+
+  const handleSubmit = async () => {
+    console.log("submit udh diprint");
+    console.log(imageBase64);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload and Display Image usign React Hook's</h1>
+      {selectedImage && (
+        <div>
+        <img alt="not fount" width={"200px"} height={"200px"} src={URL.createObjectURL(selectedImage)} />
+        <br />
+        <button onClick={()=>{
+          setSelectedImage(null)
+          // document.getElementsByName("myImage").value = null
+          ref.current.value = '';
+          
+      }}>Remove</button>
+        </div>
+      )}
+      <br />
+      <br /> 
+      <input
+        type="file"
+        name="myImage"
+        ref={ref}
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
+          reader.readAsDataURL(event.target.files[0]);
+          reader.onload = () => {setImageBase64(reader.result)}
+          
+        }}
+      />
+      <button onClick={handleSubmit}>Submit Update</button>
     </div>
   );
-}
+};
 
-export default App;
+export default UploadAndDisplayImage;
